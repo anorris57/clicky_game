7050f65193bf1d708fd5b24ff8d39f93352998b1
Angela Norris <cocoanorris@yahoo.com>
2018-09-26 12:21:36 -0400

    app has required functionality and front end is cleaned up

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <div className= "jumbotron">Welcome to clicky game</div>
+       <div className= "jumbotron">
+         <h1>Welcome to the Click Memory Game</h1><br/>
+         <p>Try and click on each image only one time. If you click all 12 images only once you win the game.</p>
+       </div>
        
        <HomeCard />
       </div>
--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -5,48 +5,74 @@ class HomeCard extends Component{
    
   state = {
     images,
-    clickedImage: false,
-    score: 0
+    score: 0,
+    clicked: []
   };
 
   handleClick = event => {
+    //capturing id of clicked image
+    let clickedimage = event;
+
+    //setting up comparison argument for conditional statement
+    let imageclickedsecondtime = this.state.clicked.indexOf(clickedimage)> -1;
+    //if image has already been clicked, score back to zero and images randomized
+    if (imageclickedsecondtime){
+      this.handleSecondClick();
+      this.handleRandom();
+      alert("Try again");
+    } else{
+    this.setState({clicked : this.state.clicked.concat(clickedimage)})
     this.handleIncrement();
     this.handleRandom();
+    }
   };
-  
+  //increments score
   handleIncrement = () => {
     this.setState({ score: this.state.score + 1 });
   };
+  //randomizes images
   handleRandom = () => {
     this.state.images.sort(function(a,b){return 0.5 - Math.random()});
+  }
+ //sets score back to zero and empties clicked array
+  handleSecondClick = () => {
+    this.setState({ score: 0});
+    this.setState({clicked: []});
+  }
+  handleWin = () => {
+    if (this.state.score === 12){
+      alert("Good job you won!");
+      this.handleRandom();
+      this.handleSecondClick();
+    }
   }
 
   render(){
     
     const imageList = images.map(image => {
       return(
-        <div>
-        <div className="container">
-        <div className="card" key={image.id}>
-          <div className="img-container">
+        <div className="card" key={image.id} 
+            id={image.id} >
             <img 
+            className="card-img-top"
             alt = {image.name}
             src = {require("../images/" + image.image)}
-            onClick={this.handleClick}
+            key={image.id} 
+            id={image.id}
+            onClick={this.handleClick.bind(this, image.id)}
+            style={{ width: 'auto'}}
             />
-          </div>
-        </div>
-        </div>
         </div>
       )
     });
     return (
-      <div className="image-list">
+      <div className=" containor image-list">
         
-      <div className="card-body">
+      <div className="card-columns">
           <p className="card-text">Click Score: {this.state.score}</p>
-        </div>
+        
         { imageList}
+        </div>
       </div>
     )
   }
